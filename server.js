0ce4c1f6381b95fcdf8236308c69453a2be6b169
gaorock530 <gaorock530@hotmail.com>
2019-08-25 13:59:32 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+
+app.set('x-powered-by', false);
+// app.use(express.static(__dirname + '/build'));
+
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, '/ssl/pay/2_pay.yingxitech.com.key')),
+  cert: fs.readFileSync(path.join(__dirname, '/ssl/pay/1_pay.yingxitech.com_bundle.crt'))
+};
+
+const server = https.createServer(options, app);
+
+
+server.post('/alipay/callback', (req, res) => {
+  res.send('<h1>Callback</h1>');
+})
+
+
+server.listen(PORT, (e) => {
+  console.log(e || `Server is running on port ${PORT}.`);
+});
